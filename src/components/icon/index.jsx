8aa265feb044223b0a8e8bf446d7da3aa6fb7dad
Bernardo Marques <bernardohmarques@gmail.com>
2021-01-11 10:38:04 -0300

    chore: created icon component

---
+++ src/components/icon/index.jsx
@@ -0,0 +1,11 @@
+import { Container, Icon } from "./style";
+
+const CarIcon = () => {
+  return (
+    <Container>
+      <Icon />
+    </Container>
+  );
+};
+
+export default CarIcon;
---
+++ src/components/icon/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding-top: 72px;
+`;
+
+// essa div é temporária, enquanto não tem o svg do icone
+export const Icon = styled.div`
+  width: 81px;
+  height: 81px;
+  background: #ff2e93;
+  border-radius: 12px;
+  margin: 0 auto;
+`;
