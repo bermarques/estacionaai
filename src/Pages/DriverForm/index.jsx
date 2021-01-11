b92022cc18ebf9510b8636503b23f705d0284221
Yuri Baldini <yuriabaldini@gmail.com>
2021-01-11 11:55:38 -0300

    feature: button style created

--- src/Pages/DriverForm/index.jsx
+++ src/Pages/DriverForm/index.jsx
@@ -2,6 +2,7 @@ import "../../Style/DriverForm/style.css";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { schema } from "../../Helpers/DriverForm/index";
+import { formButton } from "../../Style/DriverForm/style.js";
 
 const DriverForm = () => {
   const { register, handleSubmit, errors } = useForm({
@@ -14,31 +15,34 @@ const DriverForm = () => {
   };
   return (
     <div className="master">
-      <div>Driver Form</div>
       <form className="master-form" onSubmit={handleSubmit(sendForm)}>
+        Nome
         <input type="text" placeholder="Nome" name="name" ref={register} />
+        E-mail
         <input type="email" placeholder="E-mail" name="email" ref={register} />
+        Senha
         <input
           type="password"
           placeholder="Senha"
           name="password"
           ref={register}
         />
+        Confirmar Senha
         <input
           placeholder="Confirmação Senha"
           type="password"
           name="password_confirmation"
           ref={register}
         />
-
         <div>
           {errors.name?.message ||
             errors.email?.message ||
             errors.password?.message ||
             errors.password_confirmation?.message}
         </div>
-
-        <button type="submit">Enviar</button>
+        <button className="button-send" type="submit">
+          CADASTRAR
+        </button>
       </form>
     </div>
   );
