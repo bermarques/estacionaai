b54a890b34ed07580dc1be21bd2e92bf44a1c2ae
Yuri Baldini <yuriabaldini@gmail.com>
2021-01-11 15:26:37 -0300

    feature: parking forms created, yup validation created

---
+++ src/Components/ParkingForm/ParkingPrice/index.jsx
@@ -0,0 +1,62 @@
+import "../../../Style/ParkingForm/style.css";
+
+const ParkingPrice = ({ handleSubmit, sendForm, register }) => {
+  return (
+    <div className="master">
+      <form className="master-form" onSubmit={handleSubmit(sendForm)}>
+        Valor
+        <input
+          className="input-form"
+          type="number"
+          placeholder="Valor"
+          name="price"
+          ref={register}
+        />
+        Dias disponíveis
+        <input
+          className="input-form"
+          type="number"
+          placeholder="Dias disponíveis"
+          min="1"
+          name="days"
+          ref={register}
+        />
+        Vaga
+        <select className="input-form" name="park">
+          <option ref={register} value="park">
+            Selecione o tamanho da vaga.
+          </option>
+          <option ref={register} value="small">
+            Pequeno porte.
+          </option>
+          <option ref={register} value="medium">
+            Medio porte.
+          </option>
+          <option ref={register} value="big">
+            Grande porte.
+          </option>
+        </select>
+        Tipo de locação
+        <select className="input-form" name="rent-type">
+          <option ref={register} value="rent">
+            Selecione o tipo de Locação
+          </option>
+          <option ref={register} value="daily">
+            Diária
+          </option>
+          <option ref={register} value="weekly">
+            Semanal
+          </option>
+          <option ref={register} value="monthly">
+            Mensal
+          </option>
+        </select>
+        <button className="button-send" type="submit">
+          CADASTRAR
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ParkingPrice;
