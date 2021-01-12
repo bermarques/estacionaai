a1b984e1804bb85c66cb649eba72012887bbb0bf
Bernardo Marques <bernardohmarques@gmail.com>
2021-01-12 08:42:42 -0300

    chore: created register page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Switch, Route } from "react-router-dom";
 
 import Login from "./pages/login";
+import Register from "./pages/register";
+
 function App() {
   return (
     <Switch>
       <Route exact path="/login">
         <Login />
       </Route>
-      <Route exactpath="/register">{/* <Register /> */}</Route>
+      <Route exactpath="/register">
+        <Register />
+      </Route>
     </Switch>
   );
 }
---
+++ src/pages/register/index.jsx
@@ -0,0 +1,13 @@
+import CarIcon from "../../components/icon";
+import LoginTabs from "../../components/login-tabs";
+
+const Register = () => {
+  return (
+    <>
+      <CarIcon />
+      <LoginTabs />
+    </>
+  );
+};
+
+export default Register;
