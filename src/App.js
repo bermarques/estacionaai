37fe5a85b4db50abd3f0abea0ff85b14a561019e
Matheus Gasparotto Andrade <matheus.andradeg@outlook.com>
2021-01-12 13:57:34 -0300

    fix(app): rebase app.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
-import UploadImage from "./requests/uploadImages";
 
 function App() {
   return (
     <div className="App">
-      <input type="file" onChange={UploadImage} id="image" />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
