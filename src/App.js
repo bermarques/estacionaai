e0cf6ec974ac4196f4705768f1510937845da57b
Bernardo Marques <bernardohmarques@gmail.com>
2021-01-12 09:22:22 -0300

    chore: add ending bar to all pages

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import { Switch, Route } from "react-router-dom";
 
 import Login from "./pages/login";
 import Register from "./pages/register";
+import EndingBar from "./components/ending-bar";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/login">
-        <Login />
-      </Route>
-      <Route exactpath="/register">
-        <Register />
-      </Route>
-    </Switch>
+    <>
+      <Switch>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exactpath="/register">
+          <Register />
+        </Route>
+      </Switch>
+      <EndingBar />
+    </>
   );
 }
 
