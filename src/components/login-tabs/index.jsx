708f9a1f78235393888154ee0245409403095edf
Bernardo Marques <bernardohmarques@gmail.com>
2021-01-12 09:14:09 -0300

    styles: link buttons

--- src/components/login-tabs/index.jsx
+++ src/components/login-tabs/index.jsx
@@ -1,17 +1,19 @@
 import Nav from "react-bootstrap/Nav";
 import { LinkContainer } from "react-router-bootstrap";
-import { useState } from "react";
+import { ButtonStyling } from "./style";
 
 const LoginTabs = () => {
   return (
-    <Nav variant="tabs">
-      <LinkContainer to="/login">
-        <Nav.Link>Entre</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/register">
-        <Nav.Link>Registre-se</Nav.Link>
-      </LinkContainer>
-    </Nav>
+    <ButtonStyling>
+      <Nav variant="tabs">
+        <LinkContainer to="/login">
+          <Nav.Link>Entre</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/register">
+          <Nav.Link>Registre-se</Nav.Link>
+        </LinkContainer>
+      </Nav>
+    </ButtonStyling>
   );
 };
 
---
+++ src/components/login-tabs/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const ButtonStyling = styled.div`
+  .nav {
+    border-bottom: none;
+  }
+
+  .nav-link {
+    color: #a9a9a9;
+    font-family: Lato;
+    line-height: 33px;
+    font-weight: bold;
+  }
+
+  .active {
+    color: #ff2e93 !important;
+    border: none;
+    font-weight: 600;
+  }
+`;
