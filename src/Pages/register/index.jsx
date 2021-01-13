92da3214a6f96a9bbacd19099d64fe7a418e037c
Bernardo Marques <bernardohmarques@gmail.com>
2021-01-13 09:33:39 -0300

    style: ending bar wasn't showing correctly

--- src/Pages/register/index.jsx
+++ src/Pages/register/index.jsx
@@ -1,11 +1,15 @@
 import CarIcon from "../../Components/icon";
 import LoginTabs from "../../Components/login-tabs";
 
+import "../../Style/ParkingForm/style.css";
+import ParkingLocation from "../../Components/ParkingForm/ParkingLocation/index";
+
 const Register = () => {
   return (
     <>
       <CarIcon />
       <LoginTabs />
+      <ParkingLocation />
       <div className="text-center">
         <img src="/assets/img-sign1.png" alt="carinha feliz" />
       </div>
