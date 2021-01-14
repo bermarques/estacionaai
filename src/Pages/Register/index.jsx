dac3fb5e0f04532424abc68b52f4aaeac7b23c5d
Bernardo Marques <bernardohmarques@gmail.com>
2021-01-14 09:16:54 -0300

    chore: add login page with form; fix: duplicated folders

--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -1,6 +1,5 @@
 import CarIcon from "../../Components/icon";
 import LoginTabs from "../../Components/login-tabs";
-import DriverForm from "../../Components/DriverForm";
 
 import "../../Style/DriverForm/style.css";
 import DriverFormComponent from "../../Components/DriverForm/index";
@@ -10,13 +9,9 @@ const Register = () => {
     <>
       <CarIcon />
       <LoginTabs />
-<<<<<<< HEAD
       <div className="master">
         <DriverFormComponent />
       </div>
-=======
-      <DriverForm />
->>>>>>> 4cfaf0201cb4bc25c0be390c46661d401b6c7fb3
       <div className="text-center">
         <img src="/assets/img-sign1.png" alt="carinha feliz" />
       </div>
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 import GlobalStyle from "../Style/globalStyles";
-import Register from "../Pages/register";
-import Login from "../Pages/login";
+import Register from "../Pages/Register";
+import Login from "../Pages/Login";
 import EndingBar from "../Components/ending-bar";
 
 const Routes = () => {
