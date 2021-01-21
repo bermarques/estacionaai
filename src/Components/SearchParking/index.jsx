d09f9c3c3472abf932e5a3c2338998dd99f3322c
Yuri Baldini <yuriabaldini@gmail.com>
2021-01-21 18:03:29 -0300

    fix: fix on search page

--- src/Components/SearchParking/index.jsx
+++ src/Components/SearchParking/index.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { changeLoading } from "../../Store/modules/loading/actions";
 import { StyledCard, MapContainer } from "../AvailableParking/style";
 import { Card, Button } from "react-bootstrap";
+import { StyledLabel } from "../../Style/globalStyles";
 import Map from "../Maps";
 import axios from "axios";
 
@@ -38,47 +39,41 @@ const SearchParking = ({ cityes }) => {
   return (
     <>
       <div>
-        Filtros
         {parking
           .filter((citys) => citys.city === cityes)
-          .map(
-            (
-              { city, image, daily, weekly, monthly, price, showMore },
-              index
-            ) => (
-              <StyledCard key={index}>
-                <Card.Img variant="top" src={image} />
-                <Card.Body>
-                  <Card.Title>{city}</Card.Title>
-                  <Card.Subtitle>
-                    {(daily && "Locação Diária") ||
-                      (weekly && "Locação Semanal") ||
-                      (monthly && "Locação Mensal ")}
-                  </Card.Subtitle>
-                  <Card.Text>Valor: {price} R$</Card.Text>
-                  <MapContainer show={showMore}>
-                    <Map
-                      isMarkerShown
-                      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-                      loadingElement={<div style={{ height: `100%` }} />}
-                      containerElement={
-                        <div style={{ width: "100%", height: `200px` }} />
-                      }
-                      mapElement={<div style={{ height: `100%` }} />}
-                    />
-                  </MapContainer>
-                  <div className="buttons">
-                    <Button variant="primary" onClick={() => setShow(true)}>
-                      Reservar
-                    </Button>
-                    <Card.Link onClick={() => setShowMore(!showMore)}>
-                      Mais Informações
-                    </Card.Link>
-                  </div>
-                </Card.Body>
-              </StyledCard>
-            )
-          )}
+          .map(({ city, image, daily, weekly, monthly, price }, index) => (
+            <StyledCard key={index}>
+              <Card.Img variant="top" src={image} />
+              <Card.Body>
+                <Card.Title>{city}</Card.Title>
+                <Card.Subtitle>
+                  {(daily && "Locação Diária") ||
+                    (weekly && "Locação Semanal") ||
+                    (monthly && "Locação Mensal ")}
+                </Card.Subtitle>
+                <Card.Text>Valor: {price} R$</Card.Text>
+                <MapContainer show={showMore}>
+                  <Map
+                    isMarkerShown
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={
+                      <div style={{ width: "100%", height: `200px` }} />
+                    }
+                    mapElement={<div style={{ height: `100%` }} />}
+                  />
+                </MapContainer>
+                <div className="buttons">
+                  <Button variant="primary" onClick={() => setShow(true)}>
+                    Reservar
+                  </Button>
+                  <Card.Link onClick={() => setShowMore(!showMore)}>
+                    Mais Informações
+                  </Card.Link>
+                </div>
+              </Card.Body>
+            </StyledCard>
+          ))}
         <ParkingLotBooking
           show={show}
           setShow={setShow}
