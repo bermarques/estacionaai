0889c4bc21785e9bed4cd6cda3aa3cbc2bb6ac73
Matheus Gasparotto Andrade <matheus.andradeg@outlook.com>
2021-01-21 00:31:14 -0300

    feat(maps): add maps to info cards

--- src/Components/AvailableParking/index.jsx
+++ src/Components/AvailableParking/index.jsx
@@ -13,12 +13,16 @@ import ParkingLotBooking from "../ParkingLotBooking";
 import { getAddress } from "../../requests/requestAdress";
 import { useCookies } from "react-cookie";
 import { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { changeLoading } from "../../Store/modules/loading/actions";
-import { StyledCard } from "./style";
+import { StyledCard, MapContainer } from "./style";
 import { Card, Button } from "react-bootstrap";
+import Map from "../Maps";
+import axios from "axios";
 
 const AvailableParkingComponents = () => {
+  const [show, setShow] = useState(false);
+  const [showMore, setShowMore] = useState(false);
   const [city, setCity] = useState("");
   const [selectedState, setSelectedStates] = useState("");
   const [cookies] = useCookies();
@@ -34,24 +38,57 @@ const AvailableParkingComponents = () => {
       .then(parking !== [] && dispatch(changeLoading(false)));
   }, []);
 
+  // const getLocation = () => {
+  //   axios
+  //     .get(
+  //       `https://maps.googleapis.com/maps/api/geocode/json?address=curitiba&key=AIzaSyBH91yJj8VsDEmzhghkeXYpTIneKTxVlK4`
+  //     )
+  //     .then((res) => console.log(res));
+  // };
+
+  // getLocation();
+
   console.log(parking);
   return parking?.map((elmt, idx) => (
-    <StyledCard key={idx}>
-      <Card.Img variant="top" src={elmt.image} />
-      <Card.Body>
-        <Card.Title>{elmt.city}</Card.Title>
-        <Card.Subtitle>
-          {(elmt.daily && "Locação Diária") ||
-            (elmt.weekly && "Locação Semanal") ||
-            (elmt.monthly && "Locação Mensal ")}
-        </Card.Subtitle>
-        <Card.Text>Valor: {elmt.price} R$</Card.Text>
-        <div className="buttons">
-          <Button variant="primary">Go somewhere</Button>
-          <Card.Link>Mais Informações</Card.Link>
-        </div>
-      </Card.Body>
-    </StyledCard>
+    <>
+      <StyledCard key={idx}>
+        <Card.Img variant="top" src={elmt.image} />
+        <Card.Body>
+          <Card.Title>{elmt.city}</Card.Title>
+          <Card.Subtitle>
+            {(elmt.daily && "Locação Diária") ||
+              (elmt.weekly && "Locação Semanal") ||
+              (elmt.monthly && "Locação Mensal ")}
+          </Card.Subtitle>
+          <Card.Text>Valor: {elmt.price} R$</Card.Text>
+          <MapContainer show={showMore}>
+            <Map
+              isMarkerShown
+              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={
+                <div style={{ width: "100%", height: `200px` }} />
+              }
+              mapElement={<div style={{ height: `100%` }} />}
+            />
+          </MapContainer>
+          <div className="buttons">
+            <Button variant="primary" onClick={() => setShow(true)}>
+              Reservar
+            </Button>
+            <Card.Link onClick={() => setShowMore(!showMore)}>
+              Mais Informações
+            </Card.Link>
+          </div>
+        </Card.Body>
+      </StyledCard>
+      <ParkingLotBooking
+        show={show}
+        setShow={setShow}
+        data={elmt}
+        token={cookies.token}
+      />
+    </>
   ));
 };
 
--- src/Components/AvailableParking/style.js
+++ src/Components/AvailableParking/style.js
@@ -1,12 +1,28 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Card } from "react-bootstrap";
 
 export const StyledCard = styled(Card)`
   width: 90vw;
   margin-bottom: 20px;
+
   .buttons {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    button {
+      background-color: #01e6b4;
+      border: none;
+    }
   }
+`;
+
+export const MapContainer = styled.div`
+  height: 0px;
+  overflow: hidden;
+  /* transition: 1s; */
+  ${(props) =>
+    props.show &&
+    css`
+      height: 200px;
+    `}
 `;
