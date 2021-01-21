677b21976d2d64eb960198b37f18ac51d1bdb088
Yuri Baldini <yuriabaldini@gmail.com>
2021-01-21 11:06:41 -0300

    feature: creating search button

--- src/Pages/AvailableParking/index.jsx
+++ src/Pages/AvailableParking/index.jsx
@@ -1,11 +1,11 @@
 import AvailableParkingComponents from "../../Components/AvailableParking/index";
-import { States } from "../../Helpers/Available/index";
 import { useState } from "react";
-import { StyledInput, StyledSelect } from "../../Style/globalStyles";
+import { StyledInput, StyledButton } from "../../Style/globalStyles";
 
 const AvailableParking = () => {
   const [city, setCity] = useState("");
-  const [selectedState, setSelectedStates] = useState("");
+
+  const searchCity = () => {};
 
   return (
     <div>
@@ -14,15 +14,9 @@ const AvailableParking = () => {
           placeholder="Buscar por cidade"
           onChange={(e) => setCity(e.target.value)}
         />
-        <StyledSelect onChange={(e) => setSelectedStates(e.target.value)}>
-          {States.map(({ value, name }, index) => (
-            <option key={index} value={value}>
-              {name}
-            </option>
-          ))}
-        </StyledSelect>
+        <StyledButton>Buscar</StyledButton>
       </div>
-      <AvailableParkingComponents />
+      <AvailableParkingComponents city={city} />
     </div>
   );
 };
