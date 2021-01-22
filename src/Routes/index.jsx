6457a79d5ddcee509b02c9b94ba3591678e98349
Matheus Gasparotto Andrade <matheus.andradeg@outlook.com>
2021-01-21 22:21:05 -0300

    feat(my parking): create a page of my parking, with user id logged

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -14,6 +14,7 @@ import { useCookies } from "react-cookie";
 import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import Loading from "../Pages/Loading";
+import MyParking from "../Pages/MyParking";
 
 const Routes = () => {
   const { message, typeMessage } = useSelector((state) => state.errorMessage);
@@ -56,6 +57,7 @@ const Routes = () => {
               <>
                 <Route exact path="/cadastroDeVagas" component={ParkingForm} />
                 <Route exact path="/vagas" component={AvailableParking} />
+                <Route exact path="/minhasvagas" component={MyParking} />
               </>
             )}
           </Switch>
